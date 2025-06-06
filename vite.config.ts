064cde2fe894fae4e2aa740422f5fca5cc5ee6d0
rahulcse129 <rahulcse129@gmail.com>
2025-06-06 16:24:33 +0530

    New

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/latest_portfolio/',
   plugins: [react()],
   optimizeDeps: {
   include: ['lucide-react']
